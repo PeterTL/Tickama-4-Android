3d3d919e8d5af0ca39e72f6907ca393ff6c2ea1b
PeterTL <info@peterlangheinrich.de>
2013-01-24 10:51:58 +0100

    Testing with InputStream and JSON, still crashing

--- src/de/peterlangheinrich/tickama/SimpleTicker.java
+++ src/de/peterlangheinrich/tickama/SimpleTicker.java
@@ -8,10 +8,24 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+// Twitter-API (Test) BOF
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+// Twitter-API (Test) EOF
 
 public class SimpleTicker extends Activity {
 
@@ -33,6 +47,28 @@ public class SimpleTicker extends Activity {
 			txtLabel.setText("ICH BIN ONLINE.");
 		else
 			txtLabel.setText("ICH BIN NICHT ONLINE.");
+		
+		// Twitter-API (Test) BOF
+		try {
+			InputStream is = new URL("https://graph.facebook.com/19292868552").openStream();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			StringBuilder sb = new StringBuilder();
+			int cp;
+			while ((cp = rd.read()) != -1) {
+				sb.append((char) cp);
+			}
+			String jsonText = sb.toString();
+			JSONObject json = new JSONObject(jsonText);
+//			System.out.println(json.toString());
+//			System.out.println(json.get("id"));
+//			Log.v("Tickama", json.toString());
+//			Log.v("Tickama", json.get("id").toString());
+			txtLabel.setText(json.get("about").toString());
+		} catch (Exception e) {
+			txtLabel.setText(e.getMessage());
+			e.printStackTrace();
+		}
+		// Twitter-API (Test) EOF
 	}
 
 	@Override
